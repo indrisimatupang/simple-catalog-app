7b83a8156bca66ff9166aac150872b4641d83505
indrisimatupang <indri.ruth@ralali.com>
2019-01-25 12:15:54 +0700

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
-import Cards from './Card.js';
+import Card from './Card';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h2><b>SIMPLE CATALOG</b></h2>
-        </header>
-        <Cards />
-      </div>
-    );
+  state = {
+    page:1,
+    pictures: [],
+    title: '',
   }
+
+  
+  render() {
+    console.log(this.state.pictures)
+    return (
+      <div className='container'>
+        <div className='App'>
+          <header className='App-header'>
+            <h2>SIMPLE CATALOG APP</h2>
+          </header>
+          <div className='cardContainer'>
+            {this.state.pictures.map(result => 
+            <Card
+            key={result.id} 
+            url={result.productImage}
+            title= {result.productName}
+            price= {result.productPrice}
+            />)}
+          </div>
+          <div className='buttonContainer'>
+            <button onClick={this.prevPage}><b>Previous</b></button>
+            <button onClick={this.nextPage}><b>Next</b></button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  componentDidMount(){
+    this.getPhotos(1)
+  }
+  
+  getPhotos = (page) => {
+    fetch('https://product-catalog-api.herokuapp.com/products?page='+page)
+    .then(results => results.json())
+    .then(data => { 
+      this.setState({
+        pictures: data.result,
+      })
+    })
+  }
+  
+  nextPage = (page) => {
+    this.getPhotos(this.state.page+1);
+  };
+  
+  prevPage = (page) => {
+    this.getPhotos(this.state.page-1);
+  };
 }
 
 export default App;
